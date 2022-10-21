7d284e45733b734d182dc6b91b24811c5ebf3116
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-10-20 21:10:47 -0300

    Home Update
    Update Files from "Home"

--- database/db.js
+++ database/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    password: "",
-    database: "prueba",
+    password: "password",
+    database: "",
 })
 
 connection.connect((error)=>{
