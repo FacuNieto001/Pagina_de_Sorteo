5c57838793eb89fea47710021f7abedb6f20d690
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-10-27 12:38:44 -0300

    Update Home
    --Update

--- app.js
+++ app.js
@@ -46,3 +46,83 @@ app.listen(3309, (req, res)=>{
 
 
 // https://www.solumedia.com.ar/radios/8682/index.html pagina radio 
+
+/*
+app.post('/auth', (req, res)=>{
+    const user = req.body.usr;
+    const pass = req.body.password;
+    //let passwordHaash = await bcryptjs.hash(pass, 8);
+        connection.query('SELECT * FROM USERS WHERE name_user = ?', [user], (error, results) =>{
+            if(error){
+                console.log("El error que devolvió SQL es: " + error);
+                return;
+            }
+
+            if (results[0] === undefined){
+                return res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Usuario y/o contraseña incorrectos",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: 1200,
+                    ruta: ''
+                })
+                return;
+            }
+            else{
+                if (pass != results[0].passwor) {
+                    return res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "Usuario y/o contraseña incorrectos",
+                        alertIcon: "error",
+                        showConfirmButton: true,
+                        timer: 1200,
+                        ruta: ''
+                    });
+                }
+                else {
+                    req.session.name = results[0].name_user;
+                    if (results[0].privilege == 'student') {
+                        req.session.loggedin1 = true;
+                        return res.render('login', {
+                            alert: true,
+                            alertTitle: "Conexión exitosa",
+                            alertMessage: "¡Login correcto!",
+                            alertIcon: "succes",
+                            showConfirmButton: false,
+                            timer: 900,
+                            ruta: ''
+                        });
+                    }
+                    if (results[0].privilege == 'teacher') {
+                        req.session.loggedin2 = true;
+                        return res.render('login', {
+                            alert: true,
+                            alertTitle: "Conexión exitosa",
+                            alertMessage: "¡Login correcto!",
+                            alertIcon: "succes",
+                            showConfirmButton: false,
+                            timer: 900,
+                            ruta: 'Teachers'
+                        });
+                    }
+                    if (results[0].privilege == 'admin') {
+                        req.session.loggedin3 = true;
+                        return res.render('login', {
+                            alert: true,
+                            alertTitle: "Conexión exitosa",
+                            alertMessage: "¡Login correcto!",
+                            alertIcon: "succes",
+                            showConfirmButton: false,
+                            timer: 900,
+                            ruta: 'land'
+                        });
+                    }
+
+                }
+            }
+        })
+});
+*/
